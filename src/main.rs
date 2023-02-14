233b64a66e2a306de27f7e80ce3ffcac7a9cdbd4
Eric Yohansa <eric.yohansa@outlook.com>
2023-02-14 15:49:44 +0700

    Add more structs

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ pub struct Currency {
     stone: f32
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct House {
     chairs: u8,
     stove: u8,
@@ -43,6 +43,70 @@ pub struct House {
     nightstand: u8,
     window: u8,
     barrel: u8
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Skills {
+    battling: u16,
+    hunting: u16,
+    mining: u16,
+    woodcutting: u16,
+    stonecarving: u16,
+    crafting: u16,
+    enchanting: u16,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Boosts {
+    critChance: u16,
+    critDamage: u16,
+    multistrike: u16,
+    healing: u16,
+    hunting_boost: u16,
+    mining_boost: u16,
+    stonecarving_boost: u16,
+    catacombs_elemental: u16,
+    catacombs_health: u16,
+    catacombs_damage: u16,
+    catacombs_hit: u16,
+    catacombs_dodge: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Partner {
+    hunting: u16,
+    mining: u16,
+    woodcutting: u16,
+    stonecarving: u16,
+    intelligence: u16,
+    speed: u16
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CatacombBanner {
+    mobs: u16,
+    reward_multiplier: u16,
+    mob_multiplier: u16,
+    character_multiplier: u16,
+    water_resistance: u16,
+    thunder_resistance: u16,
+    fire_resistance: u16,
+    melee_resistance: u16,
+    ranged_resistance: u16,
+    elemental_conversion: u16
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CatacombCharacter {
+    health: u16,
+    damage: u16,
+    hit: u16,
+    dodge: u16,
+    crit_damage: u16,
+    crit_chance: u16,
+    water_damage: u16,
+    thunder_damage: u16,
+    fire_damage: u16
 }
 
 #[derive(Serialize, Deserialize, Debug)]
