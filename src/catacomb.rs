1f7228d5852593b723e443fad57bee7a8e3ba3c1
Eric Yohansa <eric.yohansa@outlook.com>
2023-02-17 16:01:21 +0700

    Scale monster numbers

--- src/catacomb.rs
+++ src/catacomb.rs
@@ -20,16 +20,25 @@ pub struct Monster {
     water_resistance: f32,
     fire_resistance: f32,
     thunder_resistance: f32,
-    max_health: u32,
+    max_health: f32,
 }
 
 pub fn create_monster(tier: f32, cata_params: HashMap<String, String>, character_multiplier: f32, mob_multiplier: f32, mob_debuff: f32) -> Monster {
     let mut monster = Monster::default();
-    let health = 500.0 * (tier + tier.powf(1.7)) / (1.0 + mob_multiplier) / (1.0 + mob_debuff);
-    monster.health = health.round();
-    let base_damage =
-        20.0 * (tier + tier.powf(1.7)) / (1.0 + mob_multiplier) / (1.0 + character_multiplier);
-    monster.base_damage = base_damage.round();
+    monster.health = scale_by_tier(tier, mob_multiplier, mob_debuff, 500.0).round();
+    monster.base_damage = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0).round();
+    monster.hit = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0).round();
+    monster.dodge = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0).round();
+    monster.crit_chance = scale_by_tier(tier, mob_multiplier, mob_debuff, 0.1) / 100.0;
+    monster.crit_damage = scale_by_tier(tier, mob_multiplier, mob_debuff, 0.1) /100.0;
+    monster.water_damage = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0);
+    monster.thunder_damage = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0);
+    monster.fire_damage = scale_by_tier(tier, mob_multiplier, mob_debuff, 20.0);
+    monster.damage = (monster.base_damage - monster.water_damage - monster.thunder_damage - monster.fire_damage).round();
+    monster.max_health = scale_by_tier(tier, mob_multiplier, mob_debuff, 500.0);
     return monster;
 }
 
+fn scale_by_tier(tier: f32, mob_multiplier: f32, mob_debuff: f32, value_per_tier: f32) -> f32 {
+    return value_per_tier * (tier + tier.powf(1.7)) / (1.0 + mob_multiplier) / (1.0 + mob_debuff);
+}
