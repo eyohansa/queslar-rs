5c07f59ac524f1cd83c6e6f1f14cd6d62564c95f
Eric Yohansa <eric.yohansa@outlook.com>
2023-05-03 15:42:29 +0700

    Set access to properties to public

--- src/queslar.rs
+++ src/queslar.rs
@@ -2,121 +2,131 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Player {
-    username: String,
+    pub username: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Currency {
-    gold: f32,
-    credits: u16,
-    relics: u32,
-    meat: f32,
-    iron: f32,
-    wood: f32,
-    stone: f32,
+    pub gold: f32,
+    pub credits: u16,
+    pub relics: u32,
+    pub meat: f32,
+    pub iron: f32,
+    pub wood: f32,
+    pub stone: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct House {
-    chairs: u8,
-    stove: u8,
-    sink: u8,
-    basket: u8,
-    table: u8,
-    couch: u8,
-    carpet: u8,
-    candlestick: u8,
-    pitchfork: u8,
-    shed: u8,
-    fountain: u8,
-    tools: u8,
-    bed: u8,
-    closet: u8,
-    nightstand: u8,
-    window: u8,
-    barrel: u8,
+    pub chairs: u8,
+    pub stove: u8,
+    pub sink: u8,
+    pub basket: u8,
+    pub table: u8,
+    pub couch: u8,
+    pub carpet: u8,
+    pub candlestick: u8,
+    pub pitchfork: u8,
+    pub shed: u8,
+    pub fountain: u8,
+    pub tools: u8,
+    pub bed: u8,
+    pub closet: u8,
+    pub nightstand: u8,
+    pub window: u8,
+    pub barrel: u8,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Skills {
-    battling: u16,
-    hunting: u16,
-    mining: u16,
-    woodcutting: u16,
-    stonecarving: u16,
-    crafting: u16,
-    enchanting: u16,
+    pub battling: u16,
+    pub hunting: u16,
+    pub mining: u16,
+    pub woodcutting: u16,
+    pub stonecarving: u16,
+    pub crafting: u16,
+    pub enchanting: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Boosts {
     #[serde(rename = "critChance")]
-    crit_chance: u16,
+    pub crit_chance: u16,
 
     #[serde(rename = "critDamage")]
-    crit_damage: u16,
+    pub crit_damage: u16,
 
-    multistrike: u16,
-    healing: u16,
-    hunting_boost: u16,
-    mining_boost: u16,
-    stonecarving_boost: u16,
-    catacombs_elemental: u16,
-    catacombs_health: u16,
-    catacombs_damage: u16,
-    catacombs_hit: u16,
-    catacombs_dodge: u16,
+    pub multistrike: u16,
+    pub healing: u16,
+    pub hunting_boost: u16,
+    pub mining_boost: u16,
+    pub stonecarving_boost: u16,
+    pub catacombs_elemental: u16,
+    pub catacombs_health: u16,
+    pub catacombs_damage: u16,
+    pub catacombs_hit: u16,
+    pub catacombs_dodge: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Partner {
-    hunting: u16,
-    mining: u16,
-    woodcutting: u16,
-    stonecarving: u16,
-    intelligence: u16,
-    speed: u16,
+    pub hunting: u16,
+    pub mining: u16,
+    pub woodcutting: u16,
+    pub stonecarving: u16,
+    pub intelligence: u16,
+    pub speed: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CatacombBanner {
-    mobs: u16,
-    reward_multiplier: u16,
-    mob_multiplier: u16,
-    character_multiplier: u16,
-    water_resistance: u16,
-    thunder_resistance: u16,
-    fire_resistance: u16,
-    melee_resistance: u16,
-    ranged_resistance: u16,
-    elemental_conversion: u16,
+    pub mobs: f32,
+    pub reward_multiplier: f32,
+    pub mob_multiplier: f32,
+    pub character_multiplier: f32,
+    pub water_resistance: f32,
+    pub thunder_resistance: f32,
+    pub fire_resistance: f32,
+    pub melee_resistance: f32,
+    pub ranged_resistance: f32,
+    pub elemental_conversion: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CatacombCharacter {
-    health: u16,
-    damage: u16,
-    hit: u16,
-    dodge: u16,
-    crit_damage: u16,
-    crit_chance: u16,
-    water_damage: u16,
-    thunder_damage: u16,
-    fire_damage: u16,
+    pub health: u16,
+    pub damage: u16,
+    pub hit: u16,
+    pub dodge: u16,
+    pub crit_damage: u16,
+    pub crit_chance: u16,
+    pub water_damage: u16,
+    pub thunder_damage: u16,
+    pub fire_damage: u16,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Character {
-    player: Player,
-    currency: Currency,
-    house: House,
-    skills: Skills,
-    boosts: Boosts,
-    partners: Vec<Partner>,
+    pub player: Player,
+    pub currency: Currency,
+    pub house: House,
+    pub skills: Skills,
+    pub boosts: Boosts,
+    pub partners: Vec<Partner>,
 
     #[serde(rename = "catacombBanner")]
-    catacomb_banner: CatacombBanner,
+    pub catacomb_banner: CatacombBanner,
 
     #[serde(rename = "catacombCharacter")]
-    catacomb_character: CatacombCharacter,
+    pub catacomb_character: CatacombCharacter,
 }
+
+pub struct Guardian {
+    pub health: f32,
+    pub damage: f32,
+    pub hit: f32,
+    pub dodge: f32,
+    pub crit_damage: f32,
+    pub crit_chance: f32,
+}
+
